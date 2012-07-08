1cbcc262f82f74c8d010933c9bce5a44b10336c3
Gary Fenstamaker <fenstamaker@gmail.com>
2012-07-08 15:08:55 -0400

    Inital Commit

---
+++ gen/edu/montclair/hci/drawit/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.montclair.hci.drawit;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/montclair/hci/drawit/DrawItActivity.java
@@ -0,0 +1,13 @@
+package edu.montclair.hci.drawit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawItActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
